cb4201509469405b27b096233625df475a0b4366
Joao Marcos <jao.markos@hotmail.com>
2020-06-01 20:16:25 -0300

    Update DbContext and add migrations

--- RocaWebApi.Api/ApplicationDbContext.cs
+++ RocaWebApi.Api/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
 using System;
 using Microsoft.EntityFrameworkCore;
+using RocaWebApi.Api.Features.Workers;
 
 namespace RocaWebApi.Api
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Worker> Workers { get; set; }
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
---
+++ RocaWebApi.Api/Migrations/20200601221240_AddWorkerTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace RocaWebApi.Api.Migrations
+{
+    public partial class AddWorkerTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "workers",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(maxLength: 120, nullable: false),
+                    phone = table.Column<string>(maxLength: 20, nullable: true),
+                    address = table.Column<string>(maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_workers", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "workers");
+        }
+    }
+}
