5f39469acc7dd3d5f8081a2d7141f23db54ece38
Joao Marcos <jao.markos@hotmail.com>
2020-06-06 12:44:00 -0300

    Add explanation for the test script

--- RocaWebApi.Tests/run-tests.sh
+++ RocaWebApi.Tests/run-tests.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=opencover /p:ExcludeByFile='**/Migrations/'
-reportgenerator -reports:TestResults/coverage.opencover.xml -targetdir:TestResults/CoverageReport -reporttypes:"Html;HtmlSummary"
+
+# Executes the tests, collecting Coverage,
+# outputting them in the TestResults folder with OpenCover format
+# and excluding the Migrations folder
+dotnet test \
+    /p:CollectCoverage=true \
+    /p:CoverletOutput=TestResults/ \
+    /p:CoverletOutputFormat=opencover \
+    /p:ExcludeByFile='**/Migrations/'
+
+# Generates the Coverage report in HTML using the ReportGenerator tool
+reportgenerator \
+    -reports:TestResults/coverage.opencover.xml \
+    -targetdir:TestResults/CoverageReport \
+    -reporttypes:"Html;HtmlSummary"
